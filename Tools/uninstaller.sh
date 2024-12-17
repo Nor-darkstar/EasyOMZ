f6ce529875a56d33f357f7d9770ad5894839dd7a
Nor-darkstar <Nordarkstar@gmail.com>
2024-12-17 11:55:38 +0530

    Update uninstaller.sh

--- Tools/uninstaller.sh
+++ Tools/uninstaller.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -rf $HOME/.config/zsh
-rm -rf $HOME/.zcompdump
-rm -rf $HOME/.zsh_history
-rm -rf $HOME/.zshrc
+rm -rf "$HOME/.config/zsh"
+rm -rf "$HOME/.zcompdump"
+rm -rf "$HOME/.zsh_history"
+rm -rf "$HOME/.zshrc"
 rm -rf "$HOME/.local/share/fonts/MesloLGS NF Regular.ttf"
 rm -rf "$HOME/.local/share/fonts/MesloLGS NF Italic.ttf"
 rm -rf "$HOME/.local/share/fonts/MesloLGS NF Bold.ttf"
